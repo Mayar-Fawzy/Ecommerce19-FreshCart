5854a0571ab8a511362acfb8b15dcf73f37fbfba
Mayar-Fawzy <mayarfawzy715@gmail.com>
2025-03-15 22:53:53 +0200

    Refactor navbar styles for improved layout and responsiveness; remove unused imports in NodataComponent

--- src/app/Components/nodata/nodata.component.ts
+++ src/app/Components/nodata/nodata.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { NotFoundComponent } from "../../not-found/not-found.component";
 
 @Component({
   selector: 'app-nodata',
-  imports: [NotFoundComponent],
+  imports: [],
   templateUrl: './nodata.component.html',
   styleUrl: './nodata.component.scss'
 })
